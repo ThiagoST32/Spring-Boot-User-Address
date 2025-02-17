4d80106fd8b8838a34f82c3012f24886fd627528
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-17 18:43:50 -0400

    06-Configuração-Do-Serviço-Do-Usuario-Funcionando-Perfeitamente

--- src/main/java/dio/spring/projeto/spring/user/and/address/controller/UserController.java
+++ src/main/java/dio/spring/projeto/spring/user/and/address/controller/UserController.java
@@ -1,9 +1,39 @@
 package dio.spring.projeto.spring.user.and.address.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dio.spring.projeto.spring.user.and.address.domain.User;
+import dio.spring.projeto.spring.user.and.address.dto.UserDTO;
+import dio.spring.projeto.spring.user.and.address.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    @Validated
+    public ResponseEntity<User>createUser(@RequestBody UserDTO userDTO){
+        User saveUser = this.userService.saveUser(userDTO);
+        return new ResponseEntity<>(saveUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = this.userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id){
+        User user = this.userService.getOneUser(id);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
 }
