3d1774e9e32cdc44754dc5d9b4c496185087c1be
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-17 18:44:49 -0400

    07-Configuração-Do-Serviço-Do-Usuario-Funcionando-Perfeitamente-(COMMIT ERRADO PARA GIT)

--- src/main/java/dio/spring/projeto/spring/user/and/address/service/UserService.java
+++ src/main/java/dio/spring/projeto/spring/user/and/address/service/UserService.java
@@ -1,7 +1,31 @@
 package dio.spring.projeto.spring.user.and.address.service;
 
+import dio.spring.projeto.spring.user.and.address.domain.User;
+import dio.spring.projeto.spring.user.and.address.dto.UserDTO;
+import dio.spring.projeto.spring.user.and.address.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public User saveUser(UserDTO userDTO){
+        User newUser = new User(userDTO);
+        this.userRepository.save(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+        return this.userRepository.findAll();
+    }
+
+    public User getOneUser(int id){
+        return this.userRepository.findById(id).orElseThrow( () -> new RuntimeException("User not Found!!"));
+    }
 }
