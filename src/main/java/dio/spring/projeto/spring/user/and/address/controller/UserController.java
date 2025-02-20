49a02b3ea682b258932cb20c27c71fc5ad66d872
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-19 20:44:07 -0400

    13-Adição-De-Requições-Para-Receber

--- src/main/java/dio/spring/projeto/spring/user/and/address/controller/UserController.java
+++ src/main/java/dio/spring/projeto/spring/user/and/address/controller/UserController.java
@@ -1,8 +1,12 @@
 package dio.spring.projeto.spring.user.and.address.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import dio.spring.projeto.spring.user.and.address.domain.User;
 import dio.spring.projeto.spring.user.and.address.dto.UserDTO;
+import dio.spring.projeto.spring.user.and.address.dto.updateDTO.UpdateUserDTO;
+import dio.spring.projeto.spring.user.and.address.service.AddressService;
 import dio.spring.projeto.spring.user.and.address.service.UserService;
+import dio.spring.projeto.spring.user.and.address.service.cepService.CepService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +22,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private CepService cepService;
+
+    @Autowired
+    private AddressService addressService;
+
     @PostMapping
     @Validated
-    public ResponseEntity<User>createUser(@RequestBody UserDTO userDTO){
+    public ResponseEntity<User>createUser(@RequestBody UserDTO userDTO) throws JsonProcessingException {
         User saveUser = this.userService.saveUser(userDTO);
         return new ResponseEntity<>(saveUser, HttpStatus.CREATED);
     }
@@ -35,5 +45,19 @@ public class UserController {
     public ResponseEntity<User> getUserById(@PathVariable int id){
         User user = this.userService.getOneUser(id);
         return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/checkCep/{cep}")
+    public ResponseEntity<String>checkCepUser(@PathVariable("cep") String cep){
+        String address = this.cepService.consultCep(cep);
+        this.addressService.getAddressByCep(address);
+        return new ResponseEntity<>(address, HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUserById(@PathVariable int id, @RequestBody UpdateUserDTO updateUserDTO){
+        System.err.println(updateUserDTO.address());
+        User updatedUser = this.userService.updateUser(id, updateUserDTO);
+        return new ResponseEntity<>(updatedUser, HttpStatus.ACCEPTED);
     }
 }
