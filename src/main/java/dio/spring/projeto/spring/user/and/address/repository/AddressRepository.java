f0638b05a45cb639e59aab35e17ea37ea98b71b0
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-19 20:37:26 -0400

    09-Adição-De-Anotação-Em-Repositorios

--- src/main/java/dio/spring/projeto/spring/user/and/address/repository/AddressRepository.java
+++ src/main/java/dio/spring/projeto/spring/user/and/address/repository/AddressRepository.java
@@ -2,6 +2,8 @@ package dio.spring.projeto.spring.user.and.address.repository;
 
 import dio.spring.projeto.spring.user.and.address.domain.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
--- src/main/java/dio/spring/projeto/spring/user/and/address/repository/UserRepository.java
+++ src/main/java/dio/spring/projeto/spring/user/and/address/repository/UserRepository.java
@@ -2,6 +2,10 @@ package dio.spring.projeto.spring.user.and.address.repository;
 
 import dio.spring.projeto.spring.user.and.address.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+
 }
