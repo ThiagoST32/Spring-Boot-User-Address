191b5848818a517147c471b5563d2a707bde2f5a
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-21 11:11:25 -0400

    16-Refatoração-Do-ServiceCep-Para-Implementação-E-Funções-De-Atualizar-Usuario-E-Cep-Funcionando-Corretamente

--- src/main/java/dio/spring/projeto/spring/user/and/address/service/UserService.java
+++ src/main/java/dio/spring/projeto/spring/user/and/address/service/UserService.java
@@ -3,12 +3,14 @@ package dio.spring.projeto.spring.user.and.address.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import dio.spring.projeto.spring.user.and.address.domain.Address;
 import dio.spring.projeto.spring.user.and.address.domain.User;
-import dio.spring.projeto.spring.user.and.address.dto.AddressDTO;
 import dio.spring.projeto.spring.user.and.address.dto.UserDTO;
 import dio.spring.projeto.spring.user.and.address.dto.updateDTO.UpdateUserDTO;
+import dio.spring.projeto.spring.user.and.address.repository.AddressRepository;
 import dio.spring.projeto.spring.user.and.address.repository.UserRepository;
+import dio.spring.projeto.spring.user.and.address.service.cepService.CepService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,47 +21,64 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private CepService cepService;
+
     public User saveUser(UserDTO userDTO) throws JsonProcessingException {
-        Address address = Address.fromJsonCepReceiver(String.valueOf(userDTO.address()));
-        User newUser = new User(userDTO);
+        User newUser = new User(userDTO, getAddressByCep(userDTO));
         this.userRepository.save(newUser);
         return newUser;
+    }
+
+    public Address getAddressByCep(UserDTO userDTO) throws JsonProcessingException {
+        Address address = this.cepService.buscarEnderecoPorCep(userDTO.cep());
+        address.setNumero(userDTO.numero());
+        return address;
     }
 
     public List<User> getAllUsers(){
         return this.userRepository.findAll();
     }
 
-    public User getOneUser(int id){
+    public User getUserById(int id){
         return this.userRepository.findById(id).orElseThrow( () -> new RuntimeException("User not Found!!"));
     }
 
-    public boolean checkUserExist (int id){
-       User checkUser = this.userRepository.findById(id).orElseThrow();
-       if (id == checkUser.getId()){
-            return true;
-       }
-       return false;
-    }
-
-    public User updateUser(int id, UpdateUserDTO updateUserDTO){
-        User updatedUser = new User();
-        if (this.checkUserExist(id)) {
+    @Transactional
+    public Optional<User> updateUserInfo(int id, UpdateUserDTO updateUserDTO) throws JsonProcessingException {
+        User updatedUser = this.getUserById(id);
+        if (id == updatedUser.getId()) {
+            System.err.println(updateUserDTO);
             updatedUser.setNome(updateUserDTO.nome());
             updatedUser.setSobrenome(updateUserDTO.sobrenome());
             updatedUser.setEmail(updateUserDTO.email());
             updatedUser.setTelefone(updateUserDTO.telefone());
-            AddressDTO addressDTO = updateUserDTO.address();
-            Address teste = new Address(
-                    addressDTO.rua(),
-                    addressDTO.numero(),
-                    addressDTO.bairro(),
-                    addressDTO.cep(),
-                    addressDTO.cidade(),
-                    addressDTO.estado()
-            );
+            this.updateAddressUser(updatedUser, id, updateUserDTO.cep(), updateUserDTO.numero());
             this.userRepository.save(updatedUser);
         }
-        return updatedUser;
+        return Optional.of(updatedUser);
+    }
+
+    @Transactional
+    public Address updateAddressUser(User user,int id, String cep, int numero) throws JsonProcessingException {
+        Address updatedAddress = user.getAddress();
+        Address newAddressInfo = this.cepService.buscarEnderecoPorCep(cep);
+        if (id == user.getId()){
+            updatedAddress.setNumero(numero);
+            updatedAddress.setBairro(newAddressInfo.getBairro());
+            updatedAddress.setCep(newAddressInfo.getCep());
+            updatedAddress.setLocalidade(newAddressInfo.getLocalidade());
+            updatedAddress.setLogradouro(newAddressInfo.getLogradouro());
+            updatedAddress.setUf(newAddressInfo.getUf());
+        }
+        return updatedAddress;
+    }
+
+    @Transactional
+    public void deleteUserById(int id){
+        this.userRepository.deleteById(id);
     }
 }
--- src/main/java/dio/spring/projeto/spring/user/and/address/service/cepService/CepService.java
+++ src/main/java/dio/spring/projeto/spring/user/and/address/service/cepService/CepService.java
@@ -1,13 +1,28 @@
 package dio.spring.projeto.spring.user.and.address.service.cepService;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "viaCep", url = "https://viacep.com.br/ws")
-public interface CepService {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dio.spring.projeto.spring.user.and.address.domain.Address;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-    @GetMapping("{cep}/json")
-    String consultCep(@PathVariable("cep") String cep);
+@Service
+public class CepService {
+    @Autowired
+    private final RestTemplate restTemplate;
+    @Autowired
+    private final ObjectMapper objectMapper;
 
+    public CepService(RestTemplate restTemplate, ObjectMapper objectMapper) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public Address buscarEnderecoPorCep(String cep) throws JsonProcessingException {
+        String url = "https://viacep.com.br/ws/" + cep + "/json/";
+        String jsonResponse = restTemplate.getForObject(url, String.class);
+        return objectMapper.readValue(jsonResponse, Address.class);
+    }
 }
