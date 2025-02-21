3f99cbcf10a6bcf0b9f4e37b1b4b6f7d7b99c6d1
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-21 11:12:05 -0400

    17-Criação-De-Diretório-Para-Configurações-Do-Sistema

---
+++ src/main/java/dio/spring/projeto/spring/user/and/address/config/AppConfig.java
@@ -0,0 +1,13 @@
+package dio.spring.projeto.spring.user.and.address.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
