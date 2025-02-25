557a7d5e8985368763eba0703e2e56f87490f902
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-25 17:34:46 -0400

    31-Criação-De-Querys

--- src/main/java/dio/spring/projeto/spring/user/and/address/repository/UserRepository.java
+++ src/main/java/dio/spring/projeto/spring/user/and/address/repository/UserRepository.java
@@ -4,8 +4,11 @@ import dio.spring.projeto.spring.user.and.address.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
-
+    Optional<Boolean> findByEmail(String email);
+    Optional<Boolean> findByTelefone(String telefone);
+    Optional<Boolean> findByNome(String name);
 }
