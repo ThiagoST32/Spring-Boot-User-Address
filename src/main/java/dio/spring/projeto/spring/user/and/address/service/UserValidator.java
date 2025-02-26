4121f7c6c31bca78b1e7f4083f2d8dcb89d4b182
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-02-26 19:18:20 -0400

    35-Criação-De-Um-Arquivo-Externo-Para-Validação-Das-Informções-Recebidas-Pelo-Usuario-Ao-Cadastrar-COMMIT-CERTO-COMMIT-ANTERIOR-REFERENTE-AO-ARQUIVO-DE-CEP

---
+++ src/main/java/dio/spring/projeto/spring/user/and/address/service/UserValidator.java
@@ -0,0 +1,62 @@
+package dio.spring.projeto.spring.user.and.address.service;
+
+import dio.spring.projeto.spring.user.and.address.dto.UserDTO;
+import dio.spring.projeto.spring.user.and.address.exceptions.exist.EmailExistException;
+import dio.spring.projeto.spring.user.and.address.exceptions.exist.NameExistException;
+import dio.spring.projeto.spring.user.and.address.exceptions.exist.PhoneExistException;
+import dio.spring.projeto.spring.user.and.address.exceptions.invalidFormat.InvalidEmailException;
+import dio.spring.projeto.spring.user.and.address.exceptions.invalidFormat.InvalidFormatPhoneException;
+import dio.spring.projeto.spring.user.and.address.exceptions.emptyOrBlankValues.NameIsEmptyException;
+import dio.spring.projeto.spring.user.and.address.exceptions.emptyOrBlankValues.PhoneIsEmptyException;
+import dio.spring.projeto.spring.user.and.address.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
+
+@Service
+public class UserValidator {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    private boolean emailInvalido(UserDTO userDTO){
+        String validador = "^(.+)@(\\S+)$";
+        return Pattern.compile(validador).matcher(userDTO.email()).matches();
+    }
+
+    private boolean telefoneInvalido(UserDTO userDTO){
+        String validador = "^(?=[8-9])(?=[0-9]{8}).*";
+        return Pattern.compile(validador).matcher(userDTO.telefone()).matches();
+    }
+
+    private boolean phoneExist(UserDTO userDTO){
+        return this.userRepository.existByTelefone(userDTO.telefone());
+    }
+
+    private boolean nameExist(UserDTO userDTO) {
+        return this.userRepository.existByNome(userDTO.nome());
+    }
+
+    private boolean emailExistente(UserDTO userDTO){
+        return this.userRepository.existByEmail(userDTO.email());
+    }
+
+    private boolean isEmptyName(UserDTO userDTO) { return userDTO.nome().isEmpty();}
+
+    private boolean isEmptyPhone(UserDTO userDTO) { return userDTO.telefone().isEmpty();}
+
+    public void validadorUsuarioInfo(UserDTO userDTO) {
+        if (!this.emailInvalido(userDTO)) throw new InvalidEmailException();
+        if (!this.telefoneInvalido(userDTO)) throw new InvalidFormatPhoneException();
+
+        if (this.nameExist(userDTO)) throw new NameExistException();
+        if (this.emailExistente(userDTO)) throw new EmailExistException();
+        if (this.phoneExist(userDTO)) throw new PhoneExistException();
+
+        if (this.isEmptyName(userDTO)) throw new NameIsEmptyException();
+        if (this.isEmptyPhone(userDTO)) throw new PhoneIsEmptyException();
+
+    }
+}
